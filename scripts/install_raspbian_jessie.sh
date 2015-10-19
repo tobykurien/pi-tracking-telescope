cdd8ec7d533c9af98bae8167e5200f9379a949e8
Toby Kurien <toby@tobykurien.com>
2015-10-19 18:15:33 +0200

    added new dependency yaml

--- scripts/install_raspbian_jessie.sh
+++ scripts/install_raspbian_jessie.sh
@@ -4,6 +4,11 @@ sudo apt-get update
 sudo apt-get upgrade
 sudo rpi-update
 
-sudo apt-get install libopencv-dev python-opencv libgl1-mesa-dri
+# install needed python libraries
+sudo apt-get install libopencv-dev python-opencv python-yaml
+
+# enable opengl on rpi
+sudo apt-get install libgl1-mesa-dri
 
 echo "Done, but reboot is required."
+
