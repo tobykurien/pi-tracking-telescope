5c88419556d6ad58ee03bf0dd0d15284794b79b7
Toby Kurien <toby@tobykurien.com>
2015-10-22 08:55:07 +0200

    fix mixed indentation

--- app/piscope.py
+++ app/piscope.py
@@ -9,8 +9,8 @@ def read_config(filepath):
 	config = {}
 	with codecs.open('config.yml', 'r', encoding='utf8') as f:
 		yml_dict = yaml.safe_load(f)
-     	for k in yml_dict:
-     		config[k] = yml_dict[k]    
+		for k in yml_dict:
+			config[k] = yml_dict[k]
 
 	return config
 
