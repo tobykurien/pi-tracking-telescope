292d5cd8a457d1563e05bd8371643ceeacd9f214
Toby Kurien <toby@tobykurien.com>
2016-01-07 14:01:59 +0200

    added more camera configs

--- app/piscope.py
+++ app/piscope.py
@@ -18,31 +18,32 @@ def read_config(filepath):
 if __name__ == "__main__":
 	config = read_config('config.yml')
 
+	# detect focus
 	focus = ProcessFocus()
 	focus.start()
 	
+	# stack images
 	stack = ProcessStacking()
 	stack.start()
+	showStack = False
 
-	cam = Camera(rpiCam=config['rpi_camera'], cameraNum=config['camera_number'])
+	cam = Camera(rpiCam=config['rpi_camera'], cameraNum=config['camera_number'], 
+					width=config['camera_width'], height=config['camera_height'],
+					fps=config['camera_fps'])
 	cv2.namedWindow("Image", flags=cv2.CV_WINDOW_AUTOSIZE)
-	
-	showStack = True
+		
 	while True:
 		image = cam.grabFrame()		
 
-		# detect focus
+		# do background processing
 		focus.addFrame(image)
-
-		# stack images
-		stack.addFrame(image)
-
+		if showStack: stack.addFrame(image)
 		if showStack and stack.outputFrame is not None:
 			image = stack.getFrame()
 					
 		# show the image
-		cv2.putText(image, "Focus: {:.2f} Stacking: {}".format(focus.focus, showStack), (10, 30),
-			cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255), 3)
+		cv2.putText(image, "Focus: {:.2f} Stacking: {}".format(focus.focus, showStack), 
+			(10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255), 3)
 		cv2.imshow("Image", image)
 		
 		# call waitKey otherwise image won't show. Max 60fps
