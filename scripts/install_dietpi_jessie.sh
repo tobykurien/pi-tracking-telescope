a43121e75df48558280f7e1480a5ff380b2dcbca
Toby Kurien <toby@tobykurien.com>
2016-04-13 13:53:39 +0200

    include picamera in install script

--- scripts/install_dietpi_jessie.sh
+++ scripts/install_dietpi_jessie.sh
@@ -4,9 +4,6 @@
 # install basic xserver stuff
 apt-get install xinit xterm
 
-# install fluxbox for a basic "desktop", only ~2Mb
-apt-get install fluxbox
-
 # install the rest of the needed dependencies
 ./install_raspbian_jessie.sh
 
--- scripts/install_raspbian_jessie.sh
+++ scripts/install_raspbian_jessie.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
-
 sudo apt-get update
-sudo apt-get upgrade
-sudo rpi-update
+#sudo apt-get upgrade
+#sudo rpi-update
 
 # install needed python libraries
-sudo apt-get install libopencv-dev python-opencv python-yaml
+sudo apt-get -y install libopencv-dev python-opencv python-yaml python-picamera
 
 # enable opengl on rpi
-sudo apt-get install libgl1-mesa-dri
+sudo apt-get -y install libgl1-mesa-dri
 
 echo "Done, but reboot is required."
 
