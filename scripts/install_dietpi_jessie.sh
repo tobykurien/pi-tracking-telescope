a505095240d298ddfec28027fe2e7451ea6e2f13
Toby Kurien <toby@tobykurien.com>
2016-04-20 13:35:54 +0200

    fix install script

--- scripts/install_dietpi_jessie.sh
+++ scripts/install_dietpi_jessie.sh
@@ -2,7 +2,7 @@
 # Install PiScope on DietPi (Jessie) for a lightweight installation (under 1Gb)
 
 # install basic xserver stuff
-apt-get install xinit xterm
+sudo apt-get -y install xinit xterm
 
 # install the rest of the needed dependencies
 ./install_raspbian_jessie.sh
