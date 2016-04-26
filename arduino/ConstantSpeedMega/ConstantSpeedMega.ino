53914aab8ae97e56701eaf5c8791b3b81db86003
Toby Kurien <toby@tobykurien.com>
2016-04-26 16:57:20 +0200

    restored wsad functionality

--- arduino/ConstantSpeedMega/ConstantSpeedMega.ino
+++ arduino/ConstantSpeedMega/ConstantSpeedMega.ino
@@ -14,20 +14,23 @@
 #define Y_MIN_PIN          14
 #define Y_MAX_PIN          15
 
-float stepsize = 500;
-int varDelay = 10;
+int stepsize = 100;
+int varDelay = 0;
 
 void setup() {
     pinMode(X_ENABLE_PIN, OUTPUT);
     pinMode(Y_ENABLE_PIN, OUTPUT);
-    digitalWrite(X_ENABLE_PIN, LOW);
-    digitalWrite(Y_ENABLE_PIN, LOW);
+    digitalWrite(X_ENABLE_PIN, HIGH);
+    digitalWrite(Y_ENABLE_PIN, HIGH);
 
-    Serial.begin(115200);
+    Serial.begin(9600);
     printHelp();
 }
 
 void stepX(int steps, int dir) {
+    digitalWrite(X_ENABLE_PIN, LOW);
+    digitalWrite(Y_ENABLE_PIN, LOW);
+  
     if (dir == 0) {
         digitalWrite(X_DIR_PIN, LOW);
     } else {
@@ -36,12 +39,38 @@ void stepX(int steps, int dir) {
 
     for (int x = 0; x < steps; x++) {
         digitalWrite(X_STEP_PIN, HIGH);
-        delay(varDelay);
+        //delay(varDelay);
         digitalWrite(X_STEP_PIN, LOW);
-        delay(varDelay);
+        //delay(varDelay);
 
         if(x%50==0)Serial.println(x);
     }
+
+    digitalWrite(X_ENABLE_PIN, HIGH);
+    digitalWrite(Y_ENABLE_PIN, HIGH);
+}
+
+void stepY(int steps, int dir) {
+    digitalWrite(X_ENABLE_PIN, LOW);
+    digitalWrite(Y_ENABLE_PIN, LOW);
+
+    if (dir == 0) {
+        digitalWrite(Y_DIR_PIN, LOW);
+    } else {
+        digitalWrite(Y_DIR_PIN, HIGH);
+    }
+
+    for (int x = 0; x < steps; x++) {
+        digitalWrite(Y_STEP_PIN, HIGH);
+        //delay(varDelay);
+        digitalWrite(Y_STEP_PIN, LOW);
+        //delay(varDelay);
+
+        if(x%50==0)Serial.println(x);
+    }
+
+    digitalWrite(X_ENABLE_PIN, HIGH);
+    digitalWrite(Y_ENABLE_PIN, HIGH);
 }
 
 
@@ -50,15 +79,11 @@ void printHelp(){
       Serial.println("1 Enable Motors ");
       Serial.println("0 Disable Motors ");
       Serial.println("");
-      Serial.println("2  - 9 delay from 20 to 90 ms");
-      Serial.println("u Motor 1 - CW  ");
-      Serial.println("n Motor 1 - CCW ");
-      Serial.println("t 5000 steps ");
-      Serial.println("b -5000 steps ");
-      Serial.println("o 100 steps ");
-      Serial.println("l -100 steps ");
-      Serial.println("i 10 steps");
-      Serial.println("k -10 steps");
+      Serial.println("2  - 9 stepsize from 1 to 1000");
+      Serial.println("w up");
+      Serial.println("s down");
+      Serial.println("a left");
+      Serial.println("d right");
 }
 
 void loop() {
@@ -68,6 +93,7 @@ void loop() {
         switch (r) {
             case '?':
               printHelp();
+              break;
           
             case '1':
                 digitalWrite(X_ENABLE_PIN, LOW);
@@ -78,79 +104,59 @@ void loop() {
                 digitalWrite(X_ENABLE_PIN, HIGH);
                 digitalWrite(Y_ENABLE_PIN, HIGH);
                 break;
-    
-            case 'u':
-                Serial.print("U  ");
+
+            case 'a':
+                Serial.print("A  ");
                 Serial.println(stepsize);
                 stepX(stepsize, 1);
                 break;
-            case 'n':
-                Serial.print("N  ");
+            case 'd':
+                Serial.print("D  ");
                 Serial.println(stepsize);
                 stepX(stepsize, 0);
                 break;
-            case 't':
-                Serial.println("T  5000");
-                stepX(5000, 0);
+            case 'w':
+                Serial.print("W  ");
+                Serial.println(stepsize);
+                stepY(stepsize, 1);
                 break;
-    
-            case 'b':
-                Serial.println("B -5000");
-                stepX(5000, 1);
+            case 's':
+                Serial.print("S  ");
+                Serial.println(stepsize);
+                stepY(stepsize, 0);
                 break;
-    
-            case 'o':
-                Serial.println("O 100 ");
-                stepX(1000, 0);
-                break;
-    
-            case 'l':
-                Serial.println("L -100 ");
-                stepX(1000, 1);
-                break;
-    
-    
-            case 'i':
-              Serial.println("I 10 ");
-              stepX(50, 0);
-              break;
-        
-            case 'k':
-              Serial.println("K -10 ");
-              stepX(50, 1);
-              break;
 
             case '2':
-                Serial.println("delay = 20");
-                varDelay = 20;
+                Serial.println("stepsize = 1");
+                stepsize = 1;
                 break;
             case '3':
-                Serial.println("delay = 30");
-                varDelay = 30;
+                Serial.println("stepsize = 10");
+                stepsize = 10;
                 break;
             case '4':
-                Serial.println("delay = 40");
-                varDelay = 40;
+                Serial.println("stepsize = 100");
+                stepsize = 100;
                 break;
             case '5':
-                Serial.println("delay  =  50");
-                varDelay = 50;
+                Serial.println("stepsize = 500");
+                stepsize = 500;
                 break;
             case '6':
-                Serial.println("delay  =  60");
-                varDelay = 60;
+                Serial.println("stepsize = 1,000");
+                stepsize = 1000;
                 break;
             case '7':
-                Serial.println("delay  =  70");
-                varDelay = 70;
+                Serial.println("stepsize = 5,000");
+                stepsize = 5000;
                 break;
             case '8':
-                Serial.println("delay  =  80");
-                varDelay = 80;
+                Serial.println("stepsize = 10,000");
+                stepsize = 10000;
                 break;
             case '9':
-                Serial.println("delay  =  90");
-                varDelay = 90;
+                Serial.println("stepsize = 20,000");
+                stepsize = 20000;
                 break;
 
             default:
