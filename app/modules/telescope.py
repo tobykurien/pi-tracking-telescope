d403e7e8edd7d4a11abf46bfe76d7370b53dcbec
Toby Kurien <toby@tobykurien.com>
2016-04-28 13:48:14 +0200

    added telescope control class

---
+++ app/modules/telescope.py
@@ -0,0 +1,15 @@
+class Telescope:
+    def __init__(self, port='/dev/ttyACM0'):
+        self.port = port;
+        
+    def setAlt(self, speed):
+        pass
+    
+    def setAzimuth(self, speed):
+        pass
+    
+    def start(self):
+        pass
+    
+    def stop(self):
+        pass
